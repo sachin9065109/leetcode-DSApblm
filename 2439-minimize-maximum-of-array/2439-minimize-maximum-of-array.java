81b158b7bd256c7e3fe302ea4d793cbf334addb1
sachin bhagat <sachin9065109@gmail.com>
2026-07-18 19:30:14 +0530

    Time: 5 ms (61.65%), Space: 80.1 MB (88.5%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimizeArrayValue(int[] nums) {
+        long sum = 0;
+        long ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            long need = (sum + i) / (i + 1); 
+            ans = Math.max(ans, need);
+        }
+
+        return (int) ans;
+    }
+}
