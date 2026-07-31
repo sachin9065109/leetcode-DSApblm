2ed2755ba5d2627258216931ea7776130997960b
sachin bhagat <sachin9065109@gmail.com>
2026-07-31 23:44:09 +0530

    Time: 260 ms (21.95%), Space: 118.4 MB (72.92%) - LeetHub

---
+++ 1600-throne-inheritance/1600-throne-inheritance.java
@@ -0,0 +1,47 @@
+class ThroneInheritance {
+
+    private String king;
+    private Map<String, List<String>> children;
+    private Set<String> dead;
+
+    public ThroneInheritance(String kingName) {
+        king = kingName;
+        children = new HashMap<>();
+        dead = new HashSet<>();
+    }
+
+    public void birth(String parentName, String childName) {
+        children.putIfAbsent(parentName, new ArrayList<>());
+        children.putIfAbsent(childName, new ArrayList<>());
+        children.get(parentName).add(childName);
+    }
+
+    public void death(String name) {
+        dead.add(name);
+    }
+
+    public List<String> getInheritanceOrder() {
+        List<String> ans = new ArrayList<>();
+        dfs(king, ans);
+        return ans;
+    }
+
+    private void dfs(String person, List<String> ans) {
+
+        if (!dead.contains(person)) {
+            ans.add(person);
+        }
+
+        for (String child : children.getOrDefault(person, Collections.emptyList())) {
+            dfs(child, ans);
+        }
+    }
+}
+
+/**
+ * Your ThroneInheritance object will be instantiated and called as such:
+ * ThroneInheritance obj = new ThroneInheritance(kingName);
+ * obj.birth(parentName,childName);
+ * obj.death(name);
+ * List<String> param_3 = obj.getInheritanceOrder();
+ */
