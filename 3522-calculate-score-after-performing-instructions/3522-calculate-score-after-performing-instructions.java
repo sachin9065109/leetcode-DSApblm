c79dc174438f3a6525959082a647834f00a8ebb7
sachin bhagat <sachin9065109@gmail.com>
2026-08-05 01:50:20 +0530

    Time: 2 ms (91.98%), Space: 108.6 MB (54.94%) - LeetHub

---
+++ 3522-calculate-score-after-performing-instructions/3522-calculate-score-after-performing-instructions.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long calculateScore(String[] instructions, int[] values) {
+        int n = instructions.length;
+        boolean[] visited = new boolean[n];
+
+        long score = 0;
+        int i = 0;
+
+        while (i >= 0 && i < n && !visited[i]) {
+            visited[i] = true;
+
+            if (instructions[i].equals("add")) {
+                score += values[i];
+                i++;
+            } else { // jump
+                i += values[i];
+            }
+        }
+
+        return score;
+    }
+}
