3ebb217e88201b7c689610f6f1f56b088d6c3e94
sachin bhagat <sachin9065109@gmail.com>
2026-08-07 00:01:06 +0530

    Time: 28 ms (82.81%), Space: 103.1 MB (31.12%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int[] gcdValues(int[] nums, long[] queries) {
+        int max = 0;
+        for (int x : nums) {
+            max = Math.max(max, x);
+        }
+
+        int[] freq = new int[max + 1];
+        for (int x : nums) {
+            freq[x]++;
+        }
+
+        int[] cnt = new int[max + 1];
+        for (int d = 1; d <= max; d++) {
+            for (int m = d; m <= max; m += d) {
+                cnt[d] += freq[m];
+            }
+        }
+
+        long[] exact = new long[max + 1];
+
+        for (int d = max; d >= 1; d--) {
+            long pairs = (long) cnt[d] * (cnt[d] - 1) / 2;
+            for (int m = d + d; m <= max; m += d) {
+                pairs -= exact[m];
+            }
+            exact[d] = pairs;
+        }
+
+        long[] prefix = new long[max + 1];
+        for (int i = 1; i <= max; i++) {
+            prefix[i] = prefix[i - 1] + exact[i];
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            long target = queries[i] + 1;
+            int l = 1, r = max;
+
+            while (l < r) {
+                int mid = (l + r) / 2;
+                if (prefix[mid] >= target) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            }
+
+            ans[i] = l;
+        }
+
+        return ans;
+    }
+}
