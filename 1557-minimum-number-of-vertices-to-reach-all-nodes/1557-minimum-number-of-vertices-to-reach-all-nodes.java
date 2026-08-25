a0ffd2c93600372acc4380e51816ced52cd7693d
sachin bhagat <sachin9065109@gmail.com>
2026-08-25 22:48:34 +0530

    Time: 12 ms (26.51%), Space: 104.7 MB (48.18%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public List<Integer> findSmallestSetOfVertices(
+            int n, List<List<Integer>> edges) {
+
+        int[] indegree = new int[n];
+
+        for (List<Integer> edge : edges) {
+            int from = edge.get(0);
+            int to = edge.get(1);
+
+            indegree[to]++;
+        }
+
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                answer.add(i);
+            }
+        }
+
+        return answer;
+    }
+}
